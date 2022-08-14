bfc690bbd45d8056362c4821bbd31dda18fb0326
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-14 23:07:21 +0200

    Navigation Component Added

--- app/src/main/java/com/seif/ecommerceapp/MainActivity.kt
+++ app/src/main/java/com/seif/ecommerceapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.seif.ecommerceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.seif.ecommerceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
     }
 }
