8954833b50621926090dd9613a91fa165a54aa9a
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-15 22:20:52 +0200

    Bottom Navigation Bar Added

--- app/src/main/java/com/seif/ecommerceapp/HomeActivity.kt
+++ app/src/main/java/com/seif/ecommerceapp/HomeActivity.kt
@@ -2,18 +2,49 @@ package com.seif.ecommerceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.seif.ecommerceapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
-    lateinit var binding : ActivityHomeBinding
-    lateinit var navController:NavController
+    private lateinit var binding: ActivityHomeBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.homeNavHostFragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        binding.bottomNavigationView.setupWithNavController(navController)
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.productDetailsFragment -> hideBottomNav()
+                R.id.homeFragment -> showBottomNav()
+            }
+        }
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.homeNavHostFragment)
+
+        return navController.navigateUp() ||
+                super.onSupportNavigateUp()
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNavigationView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNavigationView.visibility = View.GONE
     }
 }
--- app/src/main/java/com/seif/ecommerceapp/ui/HomeFragment.kt
+++ app/src/main/java/com/seif/ecommerceapp/ui/HomeFragment.kt
@@ -5,17 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.ui.AppBarConfiguration
 import com.seif.ecommerceapp.R
+import com.seif.ecommerceapp.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
