e8ca5c7af30debc691b588e1cef9abcd65b46ec0
MostafaNaser99 <56233361+MostafaNaser99@users.noreply.github.com>
2022-08-16 19:04:48 +0200

    cart

--- app/src/main/java/com/seif/ecommerceapp/data/remote/models/CartData.kt
+++ app/src/main/java/com/seif/ecommerceapp/data/remote/models/CartData.kt
@@ -1,4 +1,13 @@
 package com.seif.ecommerceapp.data.remote.models
 
-class CartData {
+class CartData (url:String,name:String,num_of_orders:Int,price:Double){
+    var url = url
+    var name = name
+    var num_of_orders = num_of_orders
+    var price = price
+
+    fun totalprice():Double{
+        return price * num_of_orders
+    }
+
 }
--- app/src/main/java/com/seif/ecommerceapp/utils/CartAdapter.kt
+++ app/src/main/java/com/seif/ecommerceapp/utils/CartAdapter.kt
@@ -1,4 +1,49 @@
 package com.seif.ecommerceapp.utils
 
-class CartAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.seif.ecommerceapp.R
+import com.seif.ecommerceapp.data.remote.models.CartData
+import com.seif.ecommerceapp.databinding.CartRowBinding
+
+
+class CartAdapter(private val listdata: List<CartData>):
+    RecyclerView.Adapter<CartAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private lateinit var binding : CartRowBinding
+
+        val tvName: TextView
+        val tvDetails:TextView
+        val ivclothes:ImageView
+
+        init {
+            tvName = view.findViewById<TextView>(R.id.textView2)
+            tvDetails = view.findViewById<TextView>(R.id.textView)
+            ivclothes = view.findViewById<ImageView>(R.id.imageView)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.cart_row, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val cart_item = listdata[position]
+        holder.tvName.text = cart_item.name
+        Log.d("here",cart_item.name)
+
+    }
+
+    override fun getItemCount(): Int = listdata.size
 }
+
